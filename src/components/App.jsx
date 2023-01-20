84db1587761bb4da6f68e0aeaa50e435fe35e7da
Saransh Shankar <saransh.shankar@gmail.com>
2023-01-20 16:30:49 +0000

    Added Task Update functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 
 function App() {
+  const [newTask, setTask] = React.useState("");
+  const [taskArray, setArray] = React.useState([]);
+
+  function updateTaskList(event) {
+    const taskName = event.target.value;
+    setTask(taskName);
+  }
+
+  function updateTaskArray() {
+    setArray((prevItems) => {
+      return [...prevItems, newTask];
+    });
+    setTask("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          type="text"
+          name="inputTask"
+          onChange={updateTaskList}
+          value={newTask}
+        />
+        <button onClick={updateTaskArray}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {taskArray.map((task) => {
+            return <li>{task}</li>;
+          })}
+          <li>{newTask}</li>
         </ul>
       </div>
     </div>
